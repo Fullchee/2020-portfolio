3420649a594158de3ef59f71631e6871a07ea40e
Fullchee Zhang <fullchee@gmail.com>
2019-05-06 20:48:25 -0400

    Update initial text

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,24 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar">
+            <img src={avatar} alt="" />
+          </a>
+          <h1>
+            <strong>Fullchee Zhang</strong>
+          </h1>
+          <h2>Full Stack Developer</h2>
+          <a href="">U of T CS Grad</a> with a passion for the process of
+          learning
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header

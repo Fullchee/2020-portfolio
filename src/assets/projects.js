b612e37ae70bb438af2b8c2fafa5b13d99d95b48
Fullchee Zhang <fullchee@gmail.com>
2020-07-26 13:25:32 -0400

    Get the tags looking good

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -14,7 +14,7 @@ export const CURRENT_PROJECTS = [
     alt: "chinese chess board",
     caption: "Chinese Chess with React!",
     description: "Built with React, React Testing Library, and SCSS.",
-    tags: ["Front-end", "React", "JavaScript", "React Testing Library", "SASS"],
+    tags: ["Front-end", "React", "React Testing Library", "SASS"],
   },
 ];
 export const PAST_PROJECTS = [
@@ -25,7 +25,7 @@ export const PAST_PROJECTS = [
     description: "",
     repo: "https://github.com/Fullchee/calendar-shifts",
     demo: "https://calendar-shifts.netlify.app/",
-    tags: ["React", "Front-end", "JavaScript"],
+    tags: ["React", "Front-end"],
   },
   {
     id: "fullchee-values",
@@ -35,14 +35,7 @@ export const PAST_PROJECTS = [
     caption: "Fullchee's values",
     description:
       "Give a random video of something meaningful to remind you of who you want to be",
-    tags: [
-      "GraphQL",
-      "Node.js",
-      "React",
-      "Front-end",
-      "Back-end",
-      "JavaScript",
-    ],
+    tags: ["GraphQL", "Node.js", "React", "Front-end", "Back-end"],
   },
   {
     id: "courseography",
@@ -52,7 +45,7 @@ export const PAST_PROJECTS = [
       "Improved the maintainability of a React app for future volunteers\n added integration tests \n added git hooks with husky",
     repo: "https://github.com/Courseography/courseography",
     demo: "https://courseography.cs.toronto.edu/",
-    tags: ["JavaScript", "React", "Front-end"],
+    tags: ["React", "Front-end"],
   },
   {
     id: "1",
@@ -61,7 +54,7 @@ export const PAST_PROJECTS = [
     description: "Vanilla JS site with descriptions and videos",
     repo: "https://github.com/Fullchee/dragon-boat-calls",
     demo: "https://fullchee.github.io/dragon-boat-calls",
-    tags: ["JavaScript", "CSS", "Front-end"],
+    tags: ["Front-end"],
   },
   {
     id: "2",
@@ -69,7 +62,7 @@ export const PAST_PROJECTS = [
     caption: "Car Detail Exchange",
     description: "Made with Node.js, Express.js and MySQL",
     demo: "https://hidden-shore-40090.herokuapp.com/",
-    tags: ["SQL", "Node.js", "JavaScript", "Back-end"],
+    tags: ["Node.js", "Back-end"],
   },
   {
     id: "3",
@@ -77,16 +70,7 @@ export const PAST_PROJECTS = [
     caption: "Dotfiles Repository for Ubuntu",
     description: "zsh, git, npm and other configuration files",
     repo: "https://github.com/Fullchee/dotfiles",
-    tags: ["Unix"],
-  },
-  {
-    id: "4",
-    img: null,
-    caption: "Post Install Scripts",
-    description:
-      "Scripts and checklists for setting up my dev environments for Linux, Mac and Windows.",
-    repo: "https://github.com/Fullchee/post-install-scripts",
-    tags: ["Unix"],
+    tags: [],
   },
   // {
   //   id: "5",
@@ -103,7 +87,7 @@ export const PAST_PROJECTS = [
     description: "HTML5 Game with the mouse",
     repo: "https://github.com/Fullchee/delay-the-dark-era",
     demo: "https://fullchee.github.io/delay-the-dark-era/",
-    tags: ["JavaScript", "Front-end"],
+    tags: ["Front-end"],
   },
 ];
 
